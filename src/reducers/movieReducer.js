5aa424ff8e6c8cb6cb31234ba080a1a0320f2a74
Michael Adamson <michaelnjadamson@outlook.com>
2024-02-09 22:14:47 -0500

    my add movie is working

--- src/reducers/movieReducer.js
+++ src/reducers/movieReducer.js
@@ -20,7 +20,7 @@ const reducer = (state  = initialState, action) => {
             };
             return {
                 ...state,
-                movie: [...state.movies, newMovie]
+                movies: [...state.movies, newMovie]
             }
         default:
             return state;
